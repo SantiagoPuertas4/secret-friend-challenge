ff8518473738f50a93e425a9549ac9d09c8c3401
SantiagoPuertas4 <santidoors13@gmail.com>
2025-01-21 00:38:42 -0300

    Dev (#1)
    * feat: :sparkles: add array and function to add friend names to the array
    
    * feat: :sparkles: add function to update visual friends list
    
    * feat: :sparkles: add function to raffle friends
    
    * feat: :sparkles: add function to reset friends list
    
    * feat: :sparkles: add readme

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friendsArray = [];
+let friendsInput = document.getElementById('amigo');
+let friendsList = document.getElementById('listaAmigos');
+let friendWinner = document.getElementById('resultado');
+
+function trimInterno(texto) {
+  return texto.replace(/^\s+|\s+$|(\s)\s+/g, '$1');
+}
+
+function hasSpecialCharacters(texto) {
+  return /[^a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\s]/.test(texto);
+}
+
+function generateRandomNumber(numeroMaximo) {
+  return Math.floor(Math.random() * numeroMaximo);
+}
+
+function resetInputs() {
+  friendsInput.value = '';
+  friendsList.innerHTML = '';
+}
+
+function showFriendsName() {
+  let listaHTML = '';
+  for (let i = 0; i < friendsArray.length; i++) {
+    listaHTML += `<li>${friendsArray[i]}</li>`;
+  }
+  friendsList.innerHTML = listaHTML;
+}
+
+function addFriends() {
+  if (!friendsInput.value.trim()) {
+    alert('Por favor, inserte un nombre.');
+    return;
+  }
+
+  if (hasSpecialCharacters(friendsInput.value)) {
+    alert('El nombre ingresado contiene caracteres no permitidos.');
+    return;
+  }
+
+  friendsArray.push(trimInterno(friendsInput.value));
+  resetInputs();
+  showFriendsName();
+}
+
+function raffleFriends() {
+  if (friendsArray.length === 0) {
+    alert('No ingresaste amigos para sortear');
+    return;
+  }
+
+  if (friendsArray.length === 1) {
+    alert('Solo ingresaste un amigo. Ingresa al menos dos para sortear');
+    return;
+  }
+
+  let friendNumber = generateRandomNumber(friendsArray.length);
+  friendWinner.innerHTML = `<li>El amigo ganador es${friendsArray[friendNumber]}</li>`;
+}
+
+function resetFriendsList() {
+  resetInputs();
+  friendsArray = [];
+  friendWinner.innerHTML = '';
+}
