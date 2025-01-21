2b2fa7f5f4e5c8b8d1ad3780212fcc1ff60adc6d
Santiago Puertas <santidoors13@gmail.com>
2025-01-21 00:29:43 -0300

    feat: :sparkles: add function to raffle friends

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let friendsArray = [];
 let friendsInput = document.getElementById('amigo');
 let friendsList = document.getElementById('listaAmigos');
+let friendWinner = document.getElementById('resultado');
 
 function trimInterno(texto) {
   return texto.replace(/^\s+|\s+$|(\s)\s+/g, '$1');
@@ -8,6 +9,23 @@ function trimInterno(texto) {
 
 function hasSpecialCharacters(texto) {
   return /[^a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\s]/.test(texto);
+}
+
+function generateRandomNumber(numeroMaximo) {
+  return Math.floor(Math.random() * numeroMaximo);
+}
+
+function resetInputs() {
+  friendsInput.value = '';
+  friendsList.innerHTML = '';
+}
+
+function showFriendsName() {
+  let listaHTML = '';
+  for (let i = 0; i < friendsArray.length; i++) {
+    listaHTML += `<li>${friendsArray[i]}</li>`;
+  }
+  friendsList.innerHTML = listaHTML;
 }
 
 function addFriends() {
@@ -22,12 +40,21 @@ function addFriends() {
   }
 
   friendsArray.push(trimInterno(friendsInput.value));
-  friendsInput.value = '';
-  friendsList.innerHTML = '';
+  resetInputs();
+  showFriendsName();
+}
 
-  let listaHTML = '';
-  for (let i = 0; i < friendsArray.length; i++) {
-    listaHTML += `<li>${friendsArray[i]}</li>`;
+function raffleFriends() {
+  if (friendsArray.length === 0) {
+    alert('No ingresaste amigos para sortear');
+    return;
   }
-  friendsList.innerHTML = listaHTML;
+
+  if (friendsArray.length === 1) {
+    alert('Solo ingresaste un amigo. Ingresa al menos dos para sortear');
+    return;
+  }
+
+  let friendNumber = generateRandomNumber(friendsArray.length);
+  friendWinner.innerHTML = `<li>El amigo ganador es${friendsArray[friendNumber]}</li>`;
 }
