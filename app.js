727fc525fe3b81f7b5f225448c534ec04388f6d1
Santiago Puertas <santidoors13@gmail.com>
2025-01-21 00:09:41 -0300

    feat: :sparkles: add array and function to add friend names to the array

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friendsArray = [];
+let friendsInput = document.getElementById('amigo');
+
+function trimInterno(texto) {
+  return texto.replace(/^\s+|\s+$|(\s)\s+/g, '$1');
+}
+
+function hasSpecialCharacters(texto) {
+  return /[^a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\s]/.test(texto);
+}
+
+function addFriends() {
+  if (!friendsInput.value.trim()) {
+    alert('Por favor, inserte un nombre.');
+    return;
+  }
+
+  if (hasSpecialCharacters(friendsInput.value)) {
+    alert('El nombre ingresado contiene caracteres no permitidos.');
+    return;
+  }
+
+  friendsArray.push(trimInterno(friendsInput.value));
+  friendsInput.value = '';
+}
